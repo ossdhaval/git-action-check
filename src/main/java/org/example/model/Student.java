32eec5d7e5dce5e297001babdaac1f6d47d94b86
ossdhaval <ossdhaval@outlook.com>
2021-05-12 12:21:28 +0530

    feat: start project

---
+++ src/main/java/org/example/model/Student.java
@@ -0,0 +1,15 @@
+package org.example.model;
+
+public class Student {
+
+    private String first_name;
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+}
